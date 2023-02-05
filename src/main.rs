b08dcfcba6fc22936d0bf9fb743cb9774ac1f310
sergioRedondoGlobant <sergio.redondo@globant.com>
2023-02-05 13:30:08 +0100

    Chapter 3: sing up a new subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(
+            &configuration.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
