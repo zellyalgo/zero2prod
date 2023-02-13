1373fe6990b27bbecb086b1af8f6dc9b21e99e03
sergioRedondoGlobant <sergio.redondo@globant.com>
2023-02-13 16:29:49 +0100

    Chapter 4: tracing and telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use secrecy::ExposeSecret;
+
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(
-            &configuration.database.connection_string()
+            &configuration.database.connection_string().expose_secret()
         )
         .await
         .expect("Failed to connect to Postgres.");
